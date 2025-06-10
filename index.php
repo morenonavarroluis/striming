184a6c9e9bfa9ca2568f3ef7c8ec533dcf0e5fcf
morenonavarroluis <morenoluismanu@gmail.com>
2025-06-10 16:16:56 -0400

    cambios en index

--- index.php
+++ index.php
@@ -1,100 +1,41 @@
 <?php
-//conexion
-include "config/conexion.php";
-session_start();
-if (!isset($_SESSION['id_rol'])) {
-    header("Location: login.php");
-}
-
-
-// variables de paginación
-$videos_per_page = 4; // Número de vídeos a mostrar por página
-$page = isset($_GET['page']) ? (int)$_GET['page'] : 1; // Current page
-$offset = ($page - 1) * $videos_per_page; // Calculate offset
-
-// Total number of videos
-$total_videos_query = mysqli_query($conn, "SELECT COUNT(*) as total FROM `video`");
-$total_videos = mysqli_fetch_assoc($total_videos_query)['total'];
-$total_pages = ceil($total_videos / $videos_per_page); // Total number of pages
-
-// Obtener vídeos de la página actual
-$query = mysqli_query($conn, "SELECT video_id,video_name, location FROM `video` ORDER BY `video_id` ASC LIMIT $videos_per_page OFFSET $offset");
-
-include "base/header.php";
+// Conexión
+include "roles/config/conexion.php";
 ?>
-<body class="sb-nav-fixed">
-    <?php
-    include "base/navbar.php";
-    include "modal/regis_modal.php";
-    ?>
-    <br>
-    <br>
-    <div id="layoutSidenav_content">
-        <main>
-            <div class="container-fluid px-4">
-                <h1 class="mt-4">Videos</h1>
 
-                <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" style="float: right;">Nuevo Video</button>
-                <br>
-                <br>
-             <div class="card mb-4">
-                <?php while ($fetch = mysqli_fetch_array($query)) { ?>      
-                
-                    
-                
-                    <div class="card w-55 mx-auto" style="width: 50%; margin: 20px;">
-                        <video width="100%" height="240" controls>
-                            <source src="<?php echo $fetch['location'] ?>">
-                        </video>
-                          <div class="card-body">
-                            <h5 class="card-title"><?php echo $fetch['video_name'] ?></h5>
-                            <form action="config/eliminar_videos.php" method="POST">
-                                <input type="hidden" name="id" value="<?php echo $fetch['video_id']; ?>">
-                                <input type="hidden" name="name" value="<?php echo $fetch['video_name']; ?>">
-                                <button type="submit" class="btn btn-danger center">Eliminar</button>
-                            </form>
-                        </div>
-                      
-                    </div>
-                    
-                <?php } ?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Reproductor de Video</title>
+    <!-- Enlace a Bootstrap CSS (puedes usar un CDN o descargar los archivos) -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+<link rel="stylesheet" href="roles/css/nuevo.css">
+</head>
+<body class="bg-light">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8 video-container">
+                <h2 class="text-center mb-4">Reproductor de Video</h2>
+                <video id="videoPlayer" controls>
+                    <source id="videoSource" src="" type="video/mp4">
+                    Tu navegador no soporta el elemento de video.
+                </video>
+                <div class="controls-container">
+                    <button onclick="prevVideo()">Anterior</button>
+                    <button onclick="nextVideo()">Siguiente</button>
+                    <button onclick="togglePlay()">Pausar/Reproducir</button>
+                </div>
             </div>
-               
-                <!-- Enlaces de paginación -->
-                <nav aria-label="Page navigation">
-                    <ul class="pagination justify-content-center">
-                        <?php if ($page > 1): ?>
-                            <li class="page-item">
-                                <a class="page-link" href="?page=<?php echo $page - 1; ?>">Anterior</a>
-                            </li>
-                        <?php endif; ?>
-
-                        <?php for ($i = 1; $i <= $total_pages; $i++): ?>
-                            <li class="page-item <?php echo ($i == $page) ? 'active' : ''; ?>">
-                                <a class="page-link" href="?page=<?php echo $i; ?>"><?php echo $i; ?></a>
-                            </li>
-                        <?php endfor; ?>
-
-                        <?php if ($page < $total_pages): ?>
-                            <li class="page-item">
-                                <a class="page-link" href="?page=<?php echo $page + 1; ?>">Siguiente</a>
-                            </li>
-                        <?php endif; ?>
-                    </ul>
-                </nav>
-            </div>
-        </main>
-        <?php include 'base/footer.php'; ?>
+        </div>
     </div>
-</div>
-<?php include 'base/scrit.php'; ?>
+
+    <!-- Enlace a Bootstrap JS y jQuery (necesarios para algunas funcionalidades de Bootstrap) -->
+     <?php  include "videoplayer.php";  ?>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
 </body>
 </html>
-
-
-
-                       
-                                       
-                          
-                    
-                
---
+++ roles/index.php
@@ -0,0 +1,100 @@
+<?php
+//conexion
+include "config/conexion.php";
+session_start();
+if (!isset($_SESSION['id_rol'])) {
+    header("Location: login.php");
+}
+
+
+// variables de paginación
+$videos_per_page = 4; // Número de vídeos a mostrar por página
+$page = isset($_GET['page']) ? (int)$_GET['page'] : 1; // Current page
+$offset = ($page - 1) * $videos_per_page; // Calculate offset
+
+// Total number of videos
+$total_videos_query = mysqli_query($conn, "SELECT COUNT(*) as total FROM `video`");
+$total_videos = mysqli_fetch_assoc($total_videos_query)['total'];
+$total_pages = ceil($total_videos / $videos_per_page); // Total number of pages
+
+// Obtener vídeos de la página actual
+$query = mysqli_query($conn, "SELECT video_id,video_name, location FROM `video` ORDER BY `video_id` ASC LIMIT $videos_per_page OFFSET $offset");
+
+include "base/header.php";
+?>
+<body class="sb-nav-fixed">
+    <?php
+    include "base/navbar.php";
+    include "modal/regis_modal.php";
+    ?>
+    <br>
+    <br>
+    <div id="layoutSidenav_content">
+        <main>
+            <div class="container-fluid px-4">
+                <h1 class="mt-4">Videos</h1>
+
+                <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" style="float: right;">Nuevo Video</button>
+                <br>
+                <br>
+             <div class="card mb-4">
+                <?php while ($fetch = mysqli_fetch_array($query)) { ?>      
+                
+                    
+                
+                    <div class="card w-55 mx-auto" style="width: 50%; margin: 20px;">
+                        <video width="100%" height="240" controls>
+                            <source src="<?php echo $fetch['location'] ?>">
+                        </video>
+                          <div class="card-body">
+                            <h5 class="card-title"><?php echo $fetch['video_name'] ?></h5>
+                            <form action="config/eliminar_videos.php" method="POST">
+                                <input type="hidden" name="id" value="<?php echo $fetch['video_id']; ?>">
+                                <input type="hidden" name="name" value="<?php echo $fetch['video_name']; ?>">
+                                <button type="submit" class="btn btn-danger center">Eliminar</button>
+                            </form>
+                        </div>
+                      
+                    </div>
+                    
+                <?php } ?>
+            </div>
+               
+                <!-- Enlaces de paginación -->
+                <nav aria-label="Page navigation">
+                    <ul class="pagination justify-content-center">
+                        <?php if ($page > 1): ?>
+                            <li class="page-item">
+                                <a class="page-link" href="?page=<?php echo $page - 1; ?>">Anterior</a>
+                            </li>
+                        <?php endif; ?>
+
+                        <?php for ($i = 1; $i <= $total_pages; $i++): ?>
+                            <li class="page-item <?php echo ($i == $page) ? 'active' : ''; ?>">
+                                <a class="page-link" href="?page=<?php echo $i; ?>"><?php echo $i; ?></a>
+                            </li>
+                        <?php endfor; ?>
+
+                        <?php if ($page < $total_pages): ?>
+                            <li class="page-item">
+                                <a class="page-link" href="?page=<?php echo $page + 1; ?>">Siguiente</a>
+                            </li>
+                        <?php endif; ?>
+                    </ul>
+                </nav>
+            </div>
+        </main>
+        <?php include 'base/footer.php'; ?>
+    </div>
+</div>
+<?php include 'base/scrit.php'; ?>
+</body>
+</html>
+
+
+
+                       
+                                       
+                          
+                    
+                
