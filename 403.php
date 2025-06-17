fb85fb84d68d10df5c2b64d0f0ad15c0fe1e5d9d
morenonavarroluis <morenoluismanu@gmail.com>
2025-06-17 16:17:08 -0400

    Agrego de archivo htaccess y de vista para los errores de permisos

---
+++ 403.php
@@ -0,0 +1,44 @@
+<?php
+    include "base/header.php";
+?>
+    <body>
+        <div id="layoutError">
+            <div id="layoutError_content">
+                <main>
+                    <div class="container">
+                        <div class="row justify-content-center">
+                            <div class="col-lg-6">
+                                <div class="text-center mt-4">
+                                    <h1 class="display-1">403</h1>
+                                    <p class="lead">No autorizado</p>
+                                    <p>Acceso al recurso no autirizado.</p>
+                                    <a href="/striming">
+                                        <i class="fas fa-arrow-left me-1"></i>
+                                        Regresar al inicio
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </main>
+            </div>
+            <div id="layoutError_footer">
+                <footer class="py-4 bg-light mt-auto">
+                    <div class="container-fluid px-4">
+                        <div class="d-flex align-items-center justify-content-between small">
+                            <div class="text-muted">Copyright &copy; Your Website 2025</div>
+                            <div>
+                                <a href="#">Privacy Policy</a>
+                                &middot;
+                                <a href="#">Terms &amp; Conditions</a>
+                            </div>
+                        </div>
+                    </div>
+                </footer>
+            </div>
+        </div>
+         <?php
+            include "base/script.js";
+        ?>
+    </body>
+</html>
